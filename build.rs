15c843ba225f6cf7a3f7fc242c2b05bf18302243
Ilias Garnier <igarnier@protonmail.com>
2024-02-01 20:49:20 +0100

    Fix build to support LLVM 19

--- build.rs
+++ build.rs
@@ -7,7 +7,11 @@ use std::{
     str,
 };
 
+#[cfg(feature = "llvm17-0")]
 const LLVM_MAJOR_VERSION: usize = 17;
+
+#[cfg(feature = "llvm19-0")]
+const LLVM_MAJOR_VERSION: usize = 19;
 
 fn main() {
     if let Err(error) = run() {
